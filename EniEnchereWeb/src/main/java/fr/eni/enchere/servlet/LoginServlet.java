25b05f4ccebb57897f04a35f409b843a76ebe2cd
JuV87 <99645371+JuV87@users.noreply.github.com>
2022-02-15 16:22:54 +0100

    commit ihm

--- EniEnchereWeb/src/main/java/fr/eni/enchere/servlet/LoginServlet.java
+++ EniEnchereWeb/src/main/java/fr/eni/enchere/servlet/LoginServlet.java
@@ -1,6 +1,8 @@
 package fr.eni.enchere.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,14 +30,32 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// render page
 		getServletContext().getRequestDispatcher("/LoginPage.jsp").forward(request, response);
+		
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String pseudo = request.getParameter("pseudo");
+		String motDePasse = request.getParameter("motDePasse");
+		
+		/*
+		UserAccount userAccount = DataDAO.findUser(username, password);
+
+		if (userAccount == null) {
+			String errorMessage = "Identifiant et/ou mot de passe incorrect(s)";
+
+			request.setAttribute("errorMessage", errorMessage);
+
+			RequestDispatcher dispatcher //
+					= this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView.jsp");
+
+			dispatcher.forward(request, response);
+			return;
+		}
+		*/
 	}
 
 }
