e46f1449f396b903ec03b71222e213c45c6dced0
JuV87 <99645371+JuV87@users.noreply.github.com>
2022-02-22 08:58:52 +0100

    update servlet

--- EniEnchereWeb/src/main/java/fr/eni/enchere/servlet/ProfilServlet.java
+++ EniEnchereWeb/src/main/java/fr/eni/enchere/servlet/ProfilServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import fr.eni.enchere.bll.BLLManager;
 import fr.eni.enchere.bll.BusinessException;
@@ -34,18 +35,19 @@ public class ProfilServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		UtilisateurManager profilUser = new UtilisateurManager();
-		int id = (int)request.getSession().getAttribute("no_Utilisateur");
-		Utilisateur user=null;
-		try {
-			user = profilUser.selectById(id);
-		} catch (BusinessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		request.setAttribute("profil", user);
-		request.getRequestDispatcher("/WEB-INF/Profil.jsp").forward(request,response); 
-	}
+		HttpSession session = request.getSession();
+        UtilisateurManager profilUser = new UtilisateurManager();
+        int id = (int)session.getAttribute("id");
+        Utilisateur user=null;
+        try {
+            user = profilUser.selectById(id);
+        } catch (BusinessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        request.setAttribute("profil", user);
+        request.getRequestDispatcher("/WEB-INF/Profil.jsp").forward(request,response); 
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
